4eec6fe633e64cbae1dbaf3e9476721fc52ca9ad
Dickson Lau <dickson@thelevelup.com>
2018-04-03 19:47:09 -0400

    test render

--- app/controllers/api/requested_songs_controller.rb
+++ app/controllers/api/requested_songs_controller.rb
@@ -1,8 +1,8 @@
 class Api::RequestedSongsController < Api::BaseController
   def create
-    @tracks = client.get_tracks(request.body.read)
+    # @tracks = client.get_tracks(request.body.read)
 
-    render text: "##{@tracks}"
+    render text: "#{request.body.read}"
   end
 
   private
