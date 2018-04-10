fa73e7d3c33846d617b6be037065531d914e6721
Dickson Lau <dickson@thelevelup.com>
2018-04-10 17:55:48 -0400

    omments

--- app/controllers/api/requested_songs_controller.rb
+++ app/controllers/api/requested_songs_controller.rb
@@ -1,16 +1,16 @@
 class Api::RequestedSongsController < Api::BaseController
-  before_filter :allow_only_html_requests
+  # before_filter :allow_only_html_requests
 
   def create
-    render json: {
-    "response_type": "ephemeral",
-    "text": "We found multiple songs, is it any of these?",
-    "attachments": [
-        {
-            "text":"Partly cloudy today and tomorrow"
-        }
-    ]
-}
+#     render json: {
+#     "response_type": "ephemeral",
+#     "text": "We found multiple songs, is it any of these?",
+#     "attachments": [
+#         {
+#             "text":"Partly cloudy today and tomorrow"
+#         }
+#     ]
+# }
 
     @tracks = client.get_tracks(params[:track_name])
   end
