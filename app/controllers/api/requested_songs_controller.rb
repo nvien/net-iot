e76bad66748b89e00d488bc4f59445785ada8040
Dave Ferris <dave@thelevelup.com>
2018-04-10 18:17:52 -0400

    cleanup

--- app/controllers/api/requested_songs_controller.rb
+++ app/controllers/api/requested_songs_controller.rb
@@ -1,17 +1,5 @@
 class Api::RequestedSongsController < Api::BaseController
-  # before_filter :allow_only_html_requests
-
   def create
-#     render json: {
-#     "response_type": "ephemeral",
-#     "text": "We found multiple songs, is it any of these?#{params}",
-#     "attachments": [
-#         {
-#             "text":"Partly cloudy today and tomorrow"
-#         }
-#     ]
-# }
-
     @song_request = SongRequest.create(track_name: params[:text])
   end
 
