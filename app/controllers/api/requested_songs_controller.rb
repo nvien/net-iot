1adf8525c1b7483bf6eb95732adea38a256eda22
Dickson Lau <dickson@thelevelup.com>
2018-04-10 18:46:36 -0400

    buttons

--- app/controllers/api/requested_songs_controller.rb
+++ app/controllers/api/requested_songs_controller.rb
@@ -1,52 +1,52 @@
 class Api::RequestedSongsController < Api::BaseController
   def create
-    # @song_request = SongRequest.create(track_name: params[:text], response_url: params[:response_url])
+    @song_request = SongRequest.create(track_name: params[:text], response_url: params[:response_url])
     track_name = params[:text]
     response_url = params[:response_url]
 
-    @tracks = spotify_client.get_tracks(track_name)
+    # @tracks = spotify_client.get_tracks(track_name)
 
-    body = {
-    "text": "Would you like to play a game?",
-    "attachments": [
-        {
-            "text": "Choose a game to play",
-            "fallback": "You are unable to choose a game",
-            "callback_id": "wopr_game",
-            "color": "#3AA3E3",
-            "attachment_type": "default",
-            "actions": [
-                {
-                    "name": "game",
-                    "text": "Chess",
-                    "type": "button",
-                    "value": "chess"
-                },
-                {
-                    "name": "game",
-                    "text": "Falken's Maze",
-                    "type": "button",
-                    "value": "maze"
-                },
-                {
-                    "name": "game",
-                    "text": "Thermonuclear War",
-                    "style": "danger",
-                    "type": "button",
-                    "value": "war",
-                    "confirm": {
-                        "title": "Are you sure?",
-                        "text": "Wouldn't you prefer a good game of chess?",
-                        "ok_text": "Yes",
-                        "dismiss_text": "No"
-                    }
-                }
-            ]
-        }
-    ]
-}
-
-    ExternalApiRequest.new(base_uri: response_url, http_method: :post, options: { body: body })
+#     body = {
+#     "text": "Would you like to play a game?",
+#     "attachments": [
+#         {
+#             "text": "Choose a game to play",
+#             "fallback": "You are unable to choose a game",
+#             "callback_id": "wopr_game",
+#             "color": "#3AA3E3",
+#             "attachment_type": "default",
+#             "actions": [
+#                 {
+#                     "name": "game",
+#                     "text": "Chess",
+#                     "type": "button",
+#                     "value": "chess"
+#                 },
+#                 {
+#                     "name": "game",
+#                     "text": "Falken's Maze",
+#                     "type": "button",
+#                     "value": "maze"
+#                 },
+#                 {
+#                     "name": "game",
+#                     "text": "Thermonuclear War",
+#                     "style": "danger",
+#                     "type": "button",
+#                     "value": "war",
+#                     "confirm": {
+#                         "title": "Are you sure?",
+#                         "text": "Wouldn't you prefer a good game of chess?",
+#                         "ok_text": "Yes",
+#                         "dismiss_text": "No"
+#                     }
+#                 }
+#             ]
+#         }
+#     ]
+# }
+#
+#     ExternalApiRequest.new(base_uri: response_url, http_method: :post, options: { body: body })
 
 
   end
--- app/views/api/requested_songs/create.json.jbuilder
+++ app/views/api/requested_songs/create.json.jbuilder
@@ -1,3 +1,39 @@
-json.(@song_request, :created_at, :track_name)
-
-json.text "Your request for #{@song_request.track_name} has been received!"
+{
+"text": "Would you like to play a game?",
+"attachments": [
+    {
+        "text": "Choose a game to play",
+        "fallback": "You are unable to choose a game",
+        "callback_id": "wopr_game",
+        "color": "#3AA3E3",
+        "attachment_type": "default",
+        "actions": [
+            {
+                "name": "game",
+                "text": "Chess",
+                "type": "button",
+                "value": "chess"
+            },
+            {
+                "name": "game",
+                "text": "Falken's Maze",
+                "type": "button",
+                "value": "maze"
+            },
+            {
+                "name": "game",
+                "text": "Thermonuclear War",
+                "style": "danger",
+                "type": "button",
+                "value": "war",
+                "confirm": {
+                    "title": "Are you sure?",
+                    "text": "Wouldn't you prefer a good game of chess?",
+                    "ok_text": "Yes",
+                    "dismiss_text": "No"
+                }
+            }
+        ]
+    }
+]
+}
