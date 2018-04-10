4c4b694ab0f2bc15ce7aa140e6eceb82a379416f
Dickson Lau <dickson@thelevelup.com>
2018-04-10 19:24:22 -0400

    schema

--- app/models/song_request.rb
+++ app/models/song_request.rb
@@ -1,5 +1,6 @@
 class SongRequest < ApplicationRecord
   validates :track_name, presence: :true
   validates :response_url, presence: :true
+  
   after_create { RequestSongsJob.perform_later(track_name, response_url) }
 end
