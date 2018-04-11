17b9005a135b74f1243bb10c984d5c19d64adf6d
Dickson Lau <dickson@thelevelup.com>
2018-04-11 17:19:16 -0400

    attempt to post

--- app/jobs/request_songs_job.rb
+++ app/jobs/request_songs_job.rb
@@ -2,8 +2,10 @@ class RequestSongsJob < ApplicationJob
   queue_as :default
 
   def perform(track_name, response_url)
-    tracks = spotify_client.get_tracks(track_name)
-    name = tracks.first.name
+    # tracks = spotify_client.get_tracks(track_name)
+    # name = tracks.first.name
+
+    ExternalApiRequest.new(http_method: :post, base_uri: response_url, options: {body: 'foo'})
   end
 
   private
