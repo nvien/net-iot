ca615f6b59ad08f10933012fc43d6f5524fffb09
Dickson Lau <dickson@thelevelup.com>
2018-04-10 20:08:07 -0400

    bloop

--- app/controllers/api/requested_songs_controller.rb
+++ app/controllers/api/requested_songs_controller.rb
@@ -1,50 +1,6 @@
 class Api::RequestedSongsController < Api::BaseController
   def create
     @song_request = SongRequest.create(track_name: params[:text], response_url: params[:response_url])
-
-    body = {
-    "text": "Would you like to play a game?",
-    "attachments": [
-        {
-            "text": "Choose a game to play",
-            "fallback": "You are unable to choose a game",
-            "callback_id": "wopr_game",
-            "color": "#3AA3E3",
-            "attachment_type": "default",
-            "actions": [
-                {
-                    "name": "game",
-                    "text": "Chess",
-                    "type": "button",
-                    "value": "chess"
-                },
-                {
-                    "name": "game",
-                    "text": "Falken's Maze",
-                    "type": "button",
-                    "value": "maze"
-                },
-                {
-                    "name": "game",
-                    "text": "Thermonuclear War",
-                    "style": "danger",
-                    "type": "button",
-                    "value": "war",
-                    "confirm": {
-                        "title": "Are you sure?",
-                        "text": "Wouldn't you prefer a good game of chess?",
-                        "ok_text": "Yes",
-                        "dismiss_text": "No"
-                    }
-                }
-            ]
-        }
-    ]
-    }
-
-    headers = { 'Content-type' => 'application/json' }
-
-    ExternalApiRequest.new(http_method: :post, base_uri: params[:response_url], options: {body: body, headers: headers})
   end
 
   private
--- app/models/song_request.rb
+++ app/models/song_request.rb
@@ -3,4 +3,10 @@ class SongRequest < ApplicationRecord
   validates :response_url, presence: :true
 
   after_create { RequestSongsJob.perform_later(track_name, response_url) }
+  after_create { post_something(response_url) }
+
+
+  def post_something(response_url)
+    ExternalApiRequest.new(http_method: :post, base_uri: response_url, options: {body: 'foo'})
+  end
 end
