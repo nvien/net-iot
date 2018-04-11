77f5c256d9d7a53f773922d923cb90452f68e399
Dickson Lau <dickson@thelevelup.com>
2018-04-11 18:02:48 -0400

    cross yo fingers

--- app/jobs/request_songs_job.rb
+++ app/jobs/request_songs_job.rb
@@ -5,47 +5,47 @@ class RequestSongsJob < ApplicationJob
     tracks = spotify_client.get_tracks(track_name)[0..4]
 
     Rails.logger(tracks)
-    # body = {
-    # "text": "Would you like to play a game?",
-    # "attachments": [
-    #     {
-    #         "text": "Choose a game to play",
-    #         "fallback": "You are unable to choose a game",
-    #         "callback_id": "wopr_game",
-    #         "color": "#3AA3E3",
-    #         "attachment_type": "default",
-    #         "actions": [
-    #             {
-    #                 "name": "song",
-    #                 "text": "Chess",
-    #                 "type": "button",
-    #                 "value": "trackstrack_id"
-    #             },
-    #             {
-    #                 "name": "game",
-    #                 "text": "Falken's Maze",
-    #                 "type": "button",
-    #                 "value": "maze"
-    #             },
-    #             {
-    #                 "name": "game",
-    #                 "text": "Thermonuclear War",
-    #                 "style": "danger",
-    #                 "type": "button",
-    #                 "value": "war",
-    #                 "confirm": {
-    #                     "title": "Are you sure?",
-    #                     "text": "Wouldn't you prefer a good game of chess?",
-    #                     "ok_text": "Yes",
-    #                     "dismiss_text": "No"
-    #                 }
-    #             }
-    #         ]
-    #     }
-    # ]
-    # }
+    body = {
+    "text": "Would you like to play a game?",
+    "attachments": [
+        {
+            "text": "Choose a game to play",
+            "fallback": "You are unable to choose a game",
+            "callback_id": "wopr_game",
+            "color": "#3AA3E3",
+            "attachment_type": "default",
+            "actions": [
+                {
+                    "name": "song",
+                    "text": "Chess",
+                    "type": "button",
+                    "value": "trackstrack_id"
+                },
+                {
+                    "name": "game",
+                    "text": "Falken's Maze",
+                    "type": "button",
+                    "value": "maze"
+                },
+                {
+                    "name": "game",
+                    "text": "Thermonuclear War",
+                    "style": "danger",
+                    "type": "button",
+                    "value": "war",
+                    "confirm": {
+                        "title": "Are you sure?",
+                        "text": "Wouldn't you prefer a good game of chess?",
+                        "ok_text": "Yes",
+                        "dismiss_text": "No"
+                    }
+                }
+            ]
+        }
+    ]
+  }.to_json
 
-    ExternalApiRequest.new(http_method: :post, base_uri: response_url, options: {body: name})
+    ExternalApiRequest.new(http_method: :post, base_uri: response_url, options: {body: body})
   end
 
   private
